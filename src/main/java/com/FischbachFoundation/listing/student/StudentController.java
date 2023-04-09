d9265285c329f78f7b71658e8d18e38553583024
LucaRM <lmalato@hotmail.com>
2023-04-09 20:32:42 -0300

    Added Delete function

--- src/main/java/com/FischbachFoundation/listing/student/StudentController.java
+++ src/main/java/com/FischbachFoundation/listing/student/StudentController.java
@@ -3,7 +3,9 @@ package com.FischbachFoundation.listing.student;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +31,10 @@ public class StudentController {
 	@PostMapping
 	public void registerNewStudent(@RequestBody Student student) {
 		studentService.addNewStudent(student);
+	}
+
+	@DeleteMapping(path = "{studentID}")
+	public void deleteStudent(@PathVariable("studentID") Long id) {
+		studentService.deleteStudent(id);
 	}
 }
